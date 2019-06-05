f3b6abf45c0408228e3bf8a0fe1e744d0b32c0bd
Kat Marchán <kzm@zkat.tech>
2019-06-05 13:40:56 +0200

    fix(content): make rm use our own Error

--- src/content/rm.rs
+++ src/content/rm.rs
@@ -1,9 +1,12 @@
-use crate::content::path;
-use ssri::Integrity;
-use std::io;
 use std::fs;
 use std::path::Path;
 
-pub fn rm(cache: &Path, sri: &Integrity) -> io::Result<()> {
-    fs::remove_file(path::content_path(&cache, &sri))
+use ssri::Integrity;
+
+use crate::content::path;
+use crate::errors::Error;
+
+pub fn rm(cache: &Path, sri: &Integrity) -> Result<(), Error> {
+    fs::remove_file(path::content_path(&cache, &sri))?;
+    Ok(())
 }
