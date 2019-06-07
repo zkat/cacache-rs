5a57d46b3f2e3b153f41d66f1b33b1ef3002e049
Kat Marchán <kzm@zkat.tech>
2019-06-07 01:31:14 -0700

    test: added basic benchmark test

---
+++ benches/benchmarks.rs
@@ -0,0 +1,21 @@
+use criterion::{
+    black_box,
+    Criterion,
+    criterion_group,
+    criterion_main
+};
+use cacache;
+use tempfile;
+
+fn get(c: &mut Criterion) {
+    let tmp = tempfile::tempdir().unwrap();
+    let cache = tmp.path().to_owned();
+    let data = b"hello world".to_vec();
+    let sri = cacache::put::data(&cache, "hello", data).unwrap();
+    c.bench_function("read_hash", move |b| {
+        b.iter(|| cacache::get::read_hash(black_box(&cache), black_box(&sri)))
+    });
+}
+
+criterion_group!(benches, get);
+criterion_main!(benches);
