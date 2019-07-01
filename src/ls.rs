1133de04d26a1dd6439844773ebfa5882b9faf71
Kat Marchán <kzm@zkat.tech>
2019-06-30 20:20:49 -0700

    doc(ls): document ls::all()

--- src/ls.rs
+++ src/ls.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 use crate::index;
 
+/// Returns an iterator that lists all cache index entries.
 pub fn all<P: AsRef<Path>>(cache: P) -> impl Iterator {
     index::ls(cache.as_ref())
 }
