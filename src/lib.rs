ef8d3ad0e71181292fdee182c7baab922ba74cbf
Kat Marchán <kzm@zkat.tech>
2019-10-18 21:49:32 -0400

    docs: improve root examples a bit

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,94 @@
 //! give you corrupted data, even if cache files get corrupted or manipulated.
 //!
 //! ## Examples
-//! ```
-//! use tempfile;
-//! use cacache;
-//! use async_std::task;
 //!
-//! let tmp = tempfile::tempdir().unwrap();
-//! let dir = tmp.path().to_owned();
-//! task::block_on(async {
-//!   cacache::put::data(&dir, "key", b"my-async-data").await.unwrap();
-//!   let data = cacache::get::data(&dir, "key").await.unwrap();
-//!   assert_eq!(data, b"my-async-data");
-//! })
+//! Un-suffixed APIs are all async, using
+//! [`async-std`](https://crates.io/crates/async-std). They let you put data
+//! in and get it back out -- asynchronously!
+//!
+//! ```no_run
+//! use async_attributes;
+//!
+//! #[async_attributes::main]
+//! async fn main() -> Result<(), cacache::Error> {
+//!   // Data goes in...
+//!   cacache::put::data("./my-cache", "key", b"hello").await?;
+//!
+//!   // ...data comes out!
+//!   let data = cacache::get::data("./my-cache", "key").await?;
+//!   assert_eq!(data, b"hello");
+//!
+//!   Ok(())
+//! }
 //! ```
+//!
+//! ### Lookup by hash
+//!
+//! What makes `cacache` content addressable, though, is its ability to fetch
+//! data by its "content address", which in our case is a ["subresource
+//! integrity" hash](https://crates.io/crates/ssri), which `cacache::put`
+//! conveniently returns for us. Fetching data by hash is significantly faster
+//! than doing key lookups:
+//!
+//! ```no_run
+//! use async_attributes;
+//!
+//! #[async_attributes::main]
+//! async fn main() -> Result<(), cacache::Error> {
+//!   // Data goes in...
+//!   let sri = cacache::put::data("./my-cache", "key", b"hello").await?;
+//!
+//!   // ...data gets looked up by `sri` ("Subresource Integrity").
+//!   let data = cacache::get::data_hash("./my-cache", &sri).await?;
+//!   assert_eq!(data, b"hello");
+//!
+//!   Ok(())
+//! }
+//! ```
+//!
+//! ### Large file support
+//!
+//! `cacache` supports large file reads, in both async and sync mode, through
+//! an API reminiscent of `std::fs::OpenOptions`:
+//!
+//! ```no_run
+//! use async_attributes;
+//! use async_std::prelude::*;
+//!
+//! #[async_attributes::main]
+//! async fn main() -> Result<(), cacache::Error> {
+//!   let mut fd = cacache::put::PutOpts::new().open("./my-cache", "key").await?;
+//!   for _ in 0..10 {
+//!     fd.write_all(b"very large data").await?;
+//!   }
+//!   // Data is only persisted to the cache after you do `fd.commit()`!
+//!   let sri = fd.commit().await?;
+//!   println!("integrity: {}", &sri);
+//!
+//!   let mut fd = cacache::get::open("./my-cache", "key").await?;
+//!   let mut buf = String::new();
+//!   fd.read_to_string(&mut buf).await?;
+//!
+//!   // Make sure to call `.check()` when you're done! It makes sure that what
+//!   // you just read is actually valid. `cacache` always verifies the data
+//!   // you get out is what it's supposed to be. The check is very cheap!
+//!   fd.check()?;
+//!
+//!   Ok(())
+//! }
+//! ```
+//!
+//! ### Sync API
 //!
 //! There are also sync APIs available if you don't want to use async/await:
-//! ```
-//! use cacache;
-//! # use tempfile;
-//! # let tmp = tempfile::tempdir().unwrap();
-//! let dir = tmp.path().to_owned();
-//! cacache::put::data_sync(&dir, "key", b"my-data").unwrap();
-//! let data = cacache::get::data_sync(&dir, "key").unwrap();
-//! assert_eq!(data, b"my-data");
+//!
+//! ```no_run
+//! fn main() -> Result<(), cacache::Error> {
+//!   cacache::put::data_sync("./my-cache", "key", b"my-data").unwrap();
+//!   let data = cacache::get::data_sync("./my-cache", "key").unwrap();
+//!   assert_eq!(data, b"my-data");
+//!   Ok(())
+//! }
 //! ```
 #![warn(missing_docs, missing_doc_code_examples)]
 
